639779daa2bc62275ef61b26bd1e1b64361e07a2
jaygautam13 <jaygautamlu13@gmail.com>
2026-04-03 17:36:44 +0530

    day 1 javascript

---
+++ JAVASCRIPT/DAY-1/app.js
@@ -0,0 +1,9 @@
+console.log("voting time");
+let age = 23 ;
+if (age>18) {
+    console.log("yon can vote")
+}
+
+// output :
+// voting time
+// you can vote
---
+++ JAVASCRIPT/DAY-1/app1.js
@@ -0,0 +1,12 @@
+let color = "red" ;
+if (color === "red"){
+    console.log("stop");
+}
+if (color === "yellow"){
+    console.log("wait");
+}
+if (color === "green"){
+    console.log("go");
+}
+
+// stop
