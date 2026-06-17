6121c18b92cfbad32e0db9a092f78e0cb70c894d
jay gautam <jayguatamlu13@gmail.com>
2026-06-17 23:52:17 +0530

    updated files

---
+++ JAVASCRIPT/DAY-4(Dom)/app.js
@@ -0,0 +1,29 @@
+// let heading = document.getElementById("heading");
+// console.dir(heading);
+// console.log(heading);
+
+//selecting by class name 
+let headings = document.getElementsByClassName("headings");
+console.dir(headings);
+console.log(headings);
+
+
+// selection by tagname
+
+let paras = document.getElementsByTagName("p");
+console.dir(paras);
+
+let firstel = document.querySelector("#myid");
+console.dir(firstel);
+
+let old = document.querySelector("h1")
+console.dir(old);
+
+let input = document.createElement("input");
+input.type = "text";
+
+let button = document.createElement("button");
+input.type = "text";
+
+
+
