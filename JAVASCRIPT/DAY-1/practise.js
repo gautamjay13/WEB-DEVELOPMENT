e8fc5064a19f9130695d64876771f29def4cdcb1
jay gautam <jaygautamlu13@gmail.com>
2026-07-22 11:45:11 +0530

    javascript

---
+++ JAVASCRIPT/DAY-1/practise.js
@@ -0,0 +1,30 @@
+let age = 23 ;
+age +=2 ; 
+console.log("the age is :",age) ;
+
+
+let hindi = 80 ;
+let english = 90 ;
+let math = 100; 
+let avg = (hindi+english+math)/3 ;
+console.log("the average of the :" ,avg );
+
+
+let num = 5; 
+let newnum = num++ ;
+newnum = ++num 
+console.log("the number is :",num);
+console.log("the new number is : ",newnum);
+
+let name = "jaygautam"
+console.log("the length of the string : ",name.length); 
+
+let firstname = "jay" ;
+console.log("the first letter of the name : ",firstname[0]);
+
+let lastname = "jay" ;
+console.log("the first letter of the name : ", lastname[lastname.length-1]);
+
+let output = "apna college"+123 ;
+console.log("the string output is : ",output) ;
+
---
+++ JAVASCRIPT/DAY-1/templateliterals.js
@@ -0,0 +1,32 @@
+let pencil = 10 ;
+let eraser =  5;
+console.log(`you pay ${pencil+eraser} rupees`); 
+
+let color = "red" ;
+if(color==="red") {
+    console.log("stop");
+}
+if(color==="yellow") {
+    console.log("wait");
+}
+if(color==="green") {
+    console.log("go now");
+}
+
+
+let size = "L" ;
+if (size==="XL") {
+    console.log("The prize is RS 250") ;
+} 
+else if(size==="L"){
+    console.log("The prize is RS 200") ;
+}
+else if(size==="M"){
+    console.log("The prize is RS 150") ;
+}
+else if(size==="S"){
+    console.log("The prize is RS 100") ;
+} 
+
+
+w
